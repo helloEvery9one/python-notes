592ff231575a84f0b89ab82c73ecf2a26426f72f
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2024-06-17 18:47:32 -0500

    idk

--- exercises.py
+++ exercises.py
@@ -6,7 +6,7 @@ from functools import reduce
 # print(f'your age is: {age}.')
 
 
-#------------------exercise 2: password checker--------------------
+# ------------------exercise 2: password checker--------------------
 username = 'davidgoggins'
 password = 'carryboatsandlogs'
 password_len = len(password)
@@ -15,27 +15,27 @@ hidden_password = '*' * len(password)
 # print(f'{username}, your password {hidden_password} is {password_len} letters long.')
 
 
-#------------------exercise 3: logical operators-------------------
+# ------------------exercise 3: logical operators-------------------
 is_magician = False
 is_expert = True
 
 # if is_magician and is_expert:
 #   print('you are a master magician')
-  
+
 # elif is_magician and not is_expert:
 #   print('at least you\'re getting there')
-  
+
 # elif not is_magician:
 #   print('you need magic powers')
 
-#----------------------exercise 4: counter-------------------------
-my_list = [1,2,3,4,5,6,7,8,9,10]
+# ----------------------exercise 4: counter-------------------------
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 counter = 0
 # for items in my_list:
 #   counter += items
 # print(counter)
 
-#----------------exercise 5: check for duplicates------------------
+# ----------------exercise 5: check for duplicates------------------
 li = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 duplicates = []
 # for items in li:
@@ -43,14 +43,16 @@ duplicates = []
 #     duplicates.append(items)
 # print(duplicates)
 
-#----------------------exercise 6: tesla---------------------------
-def check_driver_age(age=0): 
-  if int(age) < 18:
-    print("Sorry, you are too young to drive this car. Powering off")
-  elif int(age) > 18:
-    print("Powering On. Enjoy the ride!")
-  elif int(age) == 18:
-    print("Congratulations on your first year of driving. Enjoy the ride!")
+# ----------------------exercise 6: tesla---------------------------
+
+
+def check_driver_age(age=0):
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
 
 # checkDriverAge(90)
 # print('')
@@ -58,19 +60,21 @@ def check_driver_age(age=0):
 # print('')
 # checkDriverAge(18)
 
-#-----------exercise 7: highest even number in a list--------------
+# -----------exercise 7: highest even number in a list--------------
+
+
 def highest_even(li):
-  evens = []
-  for items in li:
-    if items % 2 == 0:
-      evens.append(items)
-      evens.sort()
-      evens.reverse()
-  print(evens[0])
-      
+    evens = []
+    for items in li:
+        if items % 2 == 0:
+            evens.append(items)
+            evens.sort()
+            evens.reverse()
+    print(evens[0])
+
 # highest_even([1,2,3,10,4,8,12,11])
 
-#-----------------------exercise 8: cats----------------------------
+# -----------------------exercise 8: cats----------------------------
 # class Cat:
 #   species = 'mammal'
 #   def __init__(self, name, age):
@@ -86,7 +90,7 @@ def highest_even(li):
 
 # print(f'Oldest Cat is {oldest_cat(cat1.age, cat2.age, cat3.age)} years old.')
 
-#----------------------exercise 9: pets-----------------------------
+# ----------------------exercise 9: pets-----------------------------
 # class Pets():
 #   animals = []
 #   def __init__(self, animals):
@@ -126,46 +130,56 @@ def highest_even(li):
 # my_pets = Pets(my_cats)
 # my_pets.walk()
 
-#-------------------exercise 10: super list-------------------------
+# -------------------exercise 10: super list-------------------------
+
+
 class SuperList(list):
 
-  def __len__(self):
-    return 1000
+    def __len__(self):
+        return 1000
 
 # superlist = SuperList()
 # print(len(superlist))
 # superlist.append(5)
 # print(superlist[0])
 
-#---------------exercise 11: map, filter, zip, reduce---------------
-#1 Capitalize all of the pet names and print the list
-my_pets = ['sisi', 'bibi', 'titi', 'carla']
-def capitalize():
-  li = []
-  for items in my_pets:
-    li.append(items.capitalize())
 
-  return li
+# ---------------exercise 11: map, filter, zip, reduce---------------
+# 1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+
+
+def capitalize():
+    li = []
+    for items in my_pets:
+        li.append(items.capitalize())
+
+    return li
+
 
 print(capitalize())
-  
 
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+
+# 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
+my_numbers = [5, 4, 3, 2, 1]
 my_numbers.sort()
 print(list(zip(my_strings, my_numbers)))
 
-#3 Filter the scores that pass over 50%
+# 3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
+
+
 def filter_scores(scores):
-  return scores < 50
+    return scores < 50
+
 
 print(list(filter(filter_scores, scores)))
 
 
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+# 4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
 def accumulator(acc, item):
     return acc + item
+
 
 print(reduce(accumulator, (my_numbers + scores)))
