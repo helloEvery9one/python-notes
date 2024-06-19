3e5e472bd044596db4a4e96ddcfe65b9f56227b8
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2024-06-19 11:33:43 -0500

    sadfadsfasd

---
+++ developer_tips.py
@@ -0,0 +1,24 @@
+# 1. don't read the python dictionary, as it is not needed to remember everything.
+
+# 2. make descriptive variables. your code should read like English.
+
+# 3. comments are a way to provide information/clarification on a piece of code.
+# make good use of them so you don't add clutter to your code.
+
+# 4. understand the different types of data structures. some data structures
+# are useful in different situations.
+
+# 5. when you don't use the variable in a for loop, name the variable '_'.
+# this tells other programmers that the variable is not going to be used.
+
+# 6. programmers should write good code. this means code should be clean, readable,
+# predictable, and should not have any repeats.
+
+# 7. when writing functions, it should do 2 things. the first thing is that it should
+# do one thing really well. the second is that it should return something.
+
+# 8. 'clean up' your code so other devs can understand your code without
+# having too much clutter.
+
+# 9. test your understanding. good programmers test their understanding
+# to grasp the concept of something better.
