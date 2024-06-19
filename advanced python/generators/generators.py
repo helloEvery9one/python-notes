e729612d512851eb7063f1759a388d01b392cfe1
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2024-06-19 17:47:47 -0500

    generators

---
+++ advanced python/generators/generators.py
@@ -0,0 +1,7 @@
+'''
+generators
+'''
+# generators are things that can generate a sequence of values over time
+# for example, range() is a generator because it generates a sequence of numbers
+for i in range(100): # keep in mind that generators generate values ONE-BY-ONE
+    print(i)
